44b1781cdd459c4beee032ee8c03b24bd6d82021
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-01-26 15:34:16 +0700

    docs: add docs for pairing lib and solidity verifier trait

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,17 @@ pub(crate) mod utils;
 
 pub mod schemes;
 
+/// Helper trait for generating library for elliptic curve group and pairing operation in Solidity.
+///
+/// This can be extended to other curve other than BN254 once precompiles are available.
+///
+/// Example:
+/// ```rust
+/// type YourCurve;
+/// impl PairingLibrary for YourCurve {
+/// // ...
+/// }
+/// ```
 pub trait PairingLibrary: PairingEngine {
     fn template(g2_addition: bool) -> String;
 
@@ -14,6 +25,7 @@ pub trait PairingLibrary: PairingEngine {
     fn g2_to_string(g2: &Self::G2Affine) -> String;
 }
 
+/// Primary trait for proving schemes for generating Solidity verifier.
 pub trait SolidityVerifier<E: PairingLibrary> {
     type Proof;
     type VerifyingKey;
