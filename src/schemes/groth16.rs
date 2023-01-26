8dac5d34c0c57b04c6efdc938176225072c81238
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-01-26 10:46:34 +0700

    feat: add groth16 verifier

--- src/schemes/groth16.rs
+++ src/schemes/groth16.rs
@@ -1,14 +1,160 @@
 use ark_ec::PairingEngine;
 use ark_groth16::{Groth16, Proof, VerifyingKey};
+use regex::Regex;
 
-use crate::{PairingLibrary, SolidityVerifier};
+use crate::{constants::TEMPLATE_PREFIX_TEXT, PairingLibrary, SolidityVerifier};
 
 impl<E: PairingEngine + PairingLibrary> SolidityVerifier<E> for Groth16<E> {
     type Proof = Proof<E>;
 
     type VerifyingKey = VerifyingKey<E>;
 
-    fn export(_vk: &Self::VerifyingKey) -> String {
-        todo!()
+    fn export(vk: &Self::VerifyingKey) -> String {
+        let mut template_text = String::from(CONTRACT_TEMPLATE);
+
+        let vk_regex = Regex::new(r#"(<%vk_[^i%]*%>)"#).unwrap();
+        let vk_gamma_abc_len_regex = Regex::new(r#"(<%vk_gamma_abc_length%>)"#).unwrap();
+        let vk_gamma_abc_repeat_regex = Regex::new(r#"(<%vk_gamma_abc_pts%>)"#).unwrap();
+        let vk_input_len_regex = Regex::new(r#"(<%vk_input_length%>)"#).unwrap();
+        let input_loop = Regex::new(r#"(<%input_loop%>)"#).unwrap();
+        let input_argument = Regex::new(r#"(<%input_argument%>)"#).unwrap();
+
+        template_text = vk_regex
+            .replace(template_text.as_str(), E::g1_to_string(&vk.alpha_g1))
+            .into_owned();
+
+        template_text = vk_regex
+            .replace(template_text.as_str(), E::g2_to_string(&vk.beta_g2))
+            .into_owned();
+
+        template_text = vk_regex
+            .replace(template_text.as_str(), E::g2_to_string(&vk.gamma_g2))
+            .into_owned();
+
+        template_text = vk_regex
+            .replace(template_text.as_str(), E::g2_to_string(&vk.delta_g2))
+            .into_owned();
+
+        let gamma_abc_count: usize = vk.gamma_abc_g1.len();
+        template_text = vk_gamma_abc_len_regex
+            .replace(
+                template_text.as_str(),
+                format!("{}", gamma_abc_count).as_str(),
+            )
+            .into_owned();
+
+        template_text = vk_input_len_regex
+            .replace(
+                template_text.as_str(),
+                format!("{}", gamma_abc_count - 1).as_str(),
+            )
+            .into_owned();
+
+        template_text = if gamma_abc_count > 1 {
+            input_loop.replace(
+                template_text.as_str(),
+                r#"
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }"#,
+            )
+        } else {
+            input_loop.replace(template_text.as_str(), "")
+        }
+        .to_string();
+
+        template_text = if gamma_abc_count > 1 {
+            input_argument.replace(
+                template_text.as_str(),
+                format!(", uint[{}] memory input", gamma_abc_count - 1).as_str(),
+            )
+        } else {
+            input_argument.replace(template_text.as_str(), "")
+        }
+        .to_string();
+
+        let mut gamma_abc_repeat_text = String::new();
+        for (i, g1) in vk.gamma_abc_g1.iter().enumerate() {
+            gamma_abc_repeat_text.push_str(
+                format!(
+                    "vk.gamma_abc[{}] = Pairing.G1Point({});",
+                    i,
+                    E::g1_to_string(g1)
+                )
+                .as_str(),
+            );
+            if i < gamma_abc_count - 1 {
+                gamma_abc_repeat_text.push_str("\n        ");
+            }
+        }
+
+        template_text = vk_gamma_abc_repeat_regex
+            .replace(template_text.as_str(), gamma_abc_repeat_text.as_str())
+            .into_owned();
+
+        let re = Regex::new(r"(?P<v>0[xX][0-9a-fA-F]{64})").unwrap();
+        template_text = re.replace_all(&template_text, "uint256($v)").to_string();
+
+        format!(
+            "{}\n{}\n{}",
+            TEMPLATE_PREFIX_TEXT,
+            E::template(false),
+            template_text
+        )
     }
 }
+
+const CONTRACT_TEMPLATE: &str = r#"
+contract Verifier {
+    using Pairing for *;
+    struct VerifyingKey {
+        Pairing.G1Point alpha;
+        Pairing.G2Point beta;
+        Pairing.G2Point gamma;
+        Pairing.G2Point delta;
+        Pairing.G1Point[] gamma_abc;
+    }
+    struct Proof {
+        Pairing.G1Point a;
+        Pairing.G2Point b;
+        Pairing.G1Point c;
+    }
+    function verifyingKey() pure internal returns (VerifyingKey memory vk) {
+        vk.alpha = Pairing.G1Point(<%vk_alpha%>);
+        vk.beta = Pairing.G2Point(<%vk_beta%>);
+        vk.gamma = Pairing.G2Point(<%vk_gamma%>);
+        vk.delta = Pairing.G2Point(<%vk_delta%>);
+        vk.gamma_abc = new Pairing.G1Point[](<%vk_gamma_abc_length%>);
+        <%vk_gamma_abc_pts%>
+    }
+    function verify(uint[] memory input, Proof memory proof) internal view returns (uint) {
+        uint256 snark_scalar_field = <%scalar_field%>;
+        VerifyingKey memory vk = verifyingKey();
+        require(input.length + 1 == vk.gamma_abc.length);
+        // Compute the linear combination vk_x
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
+        for (uint i = 0; i < input.length; i++) {
+            require(input[i] < snark_scalar_field);
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.gamma_abc[i + 1], input[i]));
+        }
+        vk_x = Pairing.addition(vk_x, vk.gamma_abc[0]);
+        if(!Pairing.pairingProd4(
+             proof.a, proof.b,
+             Pairing.negate(vk_x), vk.gamma,
+             Pairing.negate(proof.c), vk.delta,
+             Pairing.negate(vk.alpha), vk.beta)) return 1;
+        return 0;
+    }
+    function verifyTx(
+            Proof memory proof<%input_argument%>
+        ) public view returns (bool r) {
+        uint[] memory inputValues = new uint[](<%vk_input_length%>);
+        <%input_loop%>
+        if (verify(inputValues, proof) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+"#;
